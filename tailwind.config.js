09bbad6da9c91590d508d561f744486c7c92f9c9
umeshmanolkar7 <umesh_b2u49@tataneuskills.com>
2023-10-07 11:42:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      maxWidth:{
+        container:"14440px"
+      },
+      screens:{
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px"
+      }
+    },
+  },
+  plugins: [],
+}
